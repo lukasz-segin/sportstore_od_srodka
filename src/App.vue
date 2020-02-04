f275c1b563d7ac90dc0b9e2ca582b1be51aa664c
Angry Segin <lukasz-segin@o2.pl>
2020-02-04 19:37:09 +0100

    dlfkj

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view/>
+</template>
+<script>
+  // import Store from "./components/Store";
+  import {mapActions} from "vuex";
+
+  export default {
+    name: 'app',
+    // components: {Store},
+    methods: {
+      ...mapActions({
+        getData: "getData",
+        initializeCart: "cart/initializeCart"
+      })
+    },
+    created() {
+      this.getData();
+      this.initializeCart(this.$store);
+    }
+  }
+</script>
