bf2aae302960aacab1a0a384bc5e8b889bbdb721
Angry Segin <lukasz-segin@o2.pl>
2020-03-03 19:29:40 +0100

    Dodawanie strażnika trasy (route)

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,27 @@ import Checkout from "../components/Checkout";
 import OrderThanks from "../components/OrderThanks";
 import Authentication from "../components/admin/Authentication";
 import Admin from "../components/admin/Admin";
+import dataStore from "../store";
+
 Vue.use(VueRouter);
 export default new VueRouter({
   mode: "history",
-  routes:[
-    {path: "/", component:Store},
+  routes: [
+    {path: "/", component: Store},
     {path: "/cart", component: ShoppingCart},
     {path: "/checkout", component: Checkout},
     {path: "/thanks/:id", component: OrderThanks},
     {path: "/login", component: Authentication},
-    {path: "/admin", component: Admin},
+    {
+      path: "/admin", component: Admin,
+      beforeEnter(to, from, next) {
+        if (dataStore.state.auth.authenticated) {
+          next();
+        } else {
+          next("/login");
+        }
+      }
+    },
     {path: "*", redirect: "/"}
   ]
 })
